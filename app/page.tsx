41d0b1a5ea9c32c07f2dbd72daa54d93bbaa9f9d
Matthew Frieri <mat.frieri@gmail.com>
2024-09-12 22:04:17 -0400

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-black">hello world</h1>
+      <h1 className="font-bold">hello world</h1>
+      <h1 className="font-semibold  ">hello world</h1>
+      <h1 className="font-thin">hello world</h1>
+    </div>
+  );
+}
