310ece115e66941c62054fac0301a16ccb75ab4d
Matthew Frieri <mat.frieri@gmail.com>
2024-09-15 01:31:11 -0400

    initial responsive  design

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -11,22 +11,23 @@ type CardProps = {
 
 export default function Card({ name, href, tools, children }: CardProps) {
   return (
-    <div className="[perspective:750px] flex justify-center w-full">
+    <div className="[perspective:750px] flex">
       <Link href={href} target="_blank">
-        <div id="card" className="relative w-[80rem] h-[50rem]">
-          <div className="bg-gradient-to-b from-20% from-primary to-primary-dark shadow-dark shadow-xl rounded-md w-full h-[44.5rem]">
+        <div id="card" className="relative mx-auto w-[85%] h-[52rem]">
+          <div className="bg-gradient-to-b from-20% from-primary to-primary-dark shadow-dark shadow-xl rounded-md w-full h-[89%]">
             {children}
           </div>
           <h1
+            id="project-name"
             className={`absolute bottom-0 left-10 text-9xl ${bebas_neue.className}`}
           >
             {name}
           </h1>
-          <ul className="-right-28 bottom-[6.5rem] absolute">
+          <ul className="-right-[8%] bottom-[15%] absolute">
             {tools.toReversed().map((tool, index) => (
               <li
                 key={index}
-                className="flex justify-center items-center border-2 bg-secondary mt-3 border-bkg rounded w-36 h-10 font-medium text-bkg"
+                className="flex justify-center items-center border-2 bg-secondary mt-[5%] py-[5%] border-bkg rounded w-[120%] font-medium text-bkg"
               >
                 {tool}
               </li>
--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       title!.style.visibility = "visible";
     }
 
-    const section = document.getElementById("projects-section");
+    const section = document.getElementById("projects-container");
     const children = section!.children;
 
     for (let i = 0; i < children.length; i++) {
@@ -72,16 +72,20 @@ export default function Home() {
 
   return (
     <>
-      <section className="bg-gradient-to-b from-70% from-bkg to-bkg-dark pb-40">
-        <div className="mx-auto px-20 w-[115rem]">
-          <p className="mb-32 p-4 h-16 font-medium text-3xl text-center">
+      <section className="bg-gradient-to-b from-70% from-bkg to-bkg-dark">
+        <div id="title-container" className="pb-40 container">
+          <p
+            id="name"
+            className="mb-32 p-4 h-16 font-medium text-3xl text-center"
+          >
             Matthew Frieri
           </p>
           <div className="flex justify-between">
             <img
+              id="profile"
               src="profile.png"
               alt="Matthew Frieri"
-              className="bg-gradient-to-b from-20% from-primary to-primary-dark shadow-dark shadow-xl rounded-full max-h-[38rem]"
+              className="bg-gradient-to-b from-20% from-primary to-primary-dark shadow-dark shadow-xl rounded-full h-[38rem]"
             />
             <div
               className={`flex flex-col [perspective:750px] text-right text-[9.5rem] leading-[1.1] ml-auto ${bebas_neue.className}`}
@@ -101,7 +105,10 @@ export default function Home() {
               >
                 Problem Solver<span className="text-primary">.</span>
               </h1>
-              <div className="flex justify-center gap-8 mt-10 text-5xl">
+              <div
+                id="top-links"
+                className="flex justify-center gap-8 mt-10 text-5xl"
+              >
                 <Link
                   href={"https://github.com/MatthewFrieri"}
                   target="_blank"
@@ -122,162 +129,182 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="gap-x-16 grid grid-cols-[2fr,1fr] grid-rows-[5rem,1fr] bg-dark px-80 py-24">
-        <p className={`${bebas_neue.className} text-5xl text-secondary`}>
-          About Me
-        </p>
-        <p className={`${bebas_neue.className} text-5xl text-secondary`}>
-          My Go-To Tools
-        </p>
-        <p className="text-2xl">
-          Hello! I'm Matthew Frieri, a second-year student in Computer and Data
-          Science at the University of Toronto. I'm passionate about using
-          technology to solve real-world problems and positively impact
-          different industries and communities.
-        </p>
-        <div className="flex gap-8">
-          <TypescriptLogo width={60} />
-          <NextjsLogo width={60} />
-          <TailwindLogo width={60} />
-          <PythonLogo width={60} />
-          <GitLogo width={60} />
+      <section className="bg-dark">
+        <div className="gap-x-16 grid grid-cols-[2fr,1fr] grid-rows-[5rem,1fr] px-10 py-24 container">
+          <p className={`${bebas_neue.className} text-5xl text-secondary`}>
+            About Me
+          </p>
+          <p className={`${bebas_neue.className} text-5xl text-secondary`}>
+            My Go-To Tools
+          </p>
+          <p className="text-2xl">
+            Hello! I'm Matthew Frieri, a second-year student in Computer and
+            Data Science at the University of Toronto. I'm passionate about
+            using technology to solve real-world problems and positively impact
+            different industries and communities.
+          </p>
+          <div id="tools" className="flex flex-wrap gap-8">
+            <TypescriptLogo width={60} />
+            <NextjsLogo width={60} />
+            <TailwindLogo width={60} />
+            <PythonLogo width={60} />
+            <GitLogo width={60} />
+          </div>
         </div>
       </section>
 
-      <h1
-        id="projects-title"
-        className={`${bebas_neue.className} text-[12rem] text-center sticky top-[18rem] mt-32 mb-32`}
-      >
-        Projects
-      </h1>
-
-      <section
-        id="projects-section"
-        className="flex flex-col gap-[10rem] bg-gradient-to-b from-70% from-bkg to-bkg-dark pb-60 min-h-[52rem] overflow-hidden"
-      >
-        <Card
-          name="Resume Roast"
-          href="https://github.com/MatthewFrieri/IgnitionHacks_ResumeRoast"
-          tools={[
-            "NodeJS",
-            "React",
-            "Tailwind",
-            "Google AI Studio",
-            "ElevenLabs",
-          ]}
+      <section className="container">
+        <h1
+          id="projects-title"
+          className={`${bebas_neue.className} text-[12rem] text-center sticky top-[18rem] mt-32 mb-32`}
         >
-          <div className="gap-x-20 grid grid-cols-[31rem,1fr] grid-rows-2 p-16 h-full [perspective:750px]">
-            <img
-              src="projects/drakes_diss.png"
-              alt="A screenshot from the project."
-              className="shadow-top-left rounded"
-            />
-            <img
-              src="projects/kendricks_critique.png"
-              alt="A screenshot from the project."
-              className="shadow-bottom-left col-start-1 mt-auto rounded"
-            />
-            <p className="col-start-2 row-span-2 row-start-1 my-auto text-3xl">
-              Resume Roast takes the user's resume, and uses AI to generate a
-              track by Drake, which disses the user's resume in a personalized
-              and playful manner. Drake will lay down some smooth bars and sharp
-              lyrics highlighting the weaknesses in your resume. After listening
-              to the exceptional track by Toronto's very own, the user is then
-              prompted to "fix their resume." They are then treated to yet
-              another musical masterpiece by Drake's biggest rival, Kendrick
-              Lamar.
-            </p>
-          </div>
-        </Card>
-        <Card
-          name="Code Genius"
-          href="https://github.com/MatthewFrieri/HackThe6ix-CodeGenius"
-          tools={["NextJS", "TypeScript", "Tailwind", "Flask", "Meta Llama"]}
-        >
-          <div className="gap-x-20 grid grid-cols-[31rem,1fr] grid-rows-2 p-16 h-full [perspective:750px]">
-            <img
-              src="projects/code_review.png"
-              alt="A screenshot from the project."
-              className="shadow-top-left rounded"
-            />
-            <img
-              src="projects/why_score.png"
-              alt="A screenshot from the project."
-              className="shadow-bottom-left col-start-1 mt-auto rounded"
-            />
-            <p className="col-start-2 row-span-2 row-start-1 my-auto text-3xl">
-              CodeGenius starts with the user input. Users can drag-and-drop
-              either a singular code file or a larger .zip file with many
-              different code files in it. It can parse most modern programming
-              languages, but our models have been tested and prompted with
-              mostly Python3 code. Then, in seconds all files provided by the
-              user are annotated! Certain sections of the program are
-              highlighted as areas of potential confusion for someone else
-              reading it. Then, upon either hovering over (or clicking on) the
-              section of code, a more detailed annotation is provided.
-            </p>
-          </div>
-        </Card>
-        <Card
-          name="Cook Clever"
-          href="https://github.com/MatthewFrieri/HawkHacks_CookClever"
-          tools={["React", "Flask", "Google AI Studio", "MongoDB", "Neurelo"]}
-        >
-          <div className="flex items-center gap-x-10 p-16 h-full [perspective:750px]">
-            <img
-              src="projects/login.png"
-              alt="A screenshot from the project."
-              className="shadow-left rounded h-[34rem]"
-            />
-            <img
-              src="projects/image_analysis.png"
-              alt="A screenshot from the project."
-              className="shadow-left rounded h-[34rem]"
-            />
-            <p className="my-auto text-3xl">
-              CookClever is an innovative web app designed to help people learn
-              how to cook. It offers a wide range of recipes with detailed,
-              step-by-step instructions to guide users through the cooking
-              process. At each step, users are prompted to take a picture of
-              their progress. Using AI, CookClever analyzes these images and
-              provides tailored feedback to help users improve their cooking
-              skills.
-            </p>
-          </div>
-        </Card>
+          Projects
+        </h1>
       </section>
 
-      <section className="flex justify-center items-center gap-8 bg-dark h-60 text-4xl">
-        <div className="border-secondary mr-10 border w-80" />
-        <Link
-          href={"mailto: mat.frieri@gmail.com"}
-          target="_blank"
-          className="hover:text-secondary transition-all hover:scale-110"
+      <section className="bg-gradient-to-b from-70% from-bkg to-bkg-dark">
+        <div
+          id="projects-container"
+          className="flex flex-col gap-[10rem] px-[2%] pb-60 max-w-none overflow-hidden container"
         >
-          <FontAwesomeIcon icon={faEnvelope} />
-        </Link>
-        <Link
-          href={"https://github.com/MatthewFrieri"}
-          target="_blank"
-          className="hover:text-secondary transition-all hover:scale-110"
-        >
-          <FontAwesomeIcon icon={faGithub} />
-        </Link>
-        <Link
-          href={"https://www.linkedin.com/in/matthew-frieri"}
-          target="_blank"
-          className="hover:text-secondary transition-all hover:scale-110"
-        >
-          <FontAwesomeIcon icon={faLinkedin} />
-        </Link>
-        <Link
-          href={"Resume.pdf"}
-          target="_blank"
-          className="hover:text-secondary transition-all hover:scale-110"
-        >
-          <FontAwesomeIcon icon={faNewspaper} />
-        </Link>
-        <div className="border-secondary ml-10 border w-80" />
+          <Card
+            name="Resume Roast"
+            href="https://github.com/MatthewFrieri/IgnitionHacks_ResumeRoast"
+            tools={[
+              "NodeJS",
+              "React",
+              "Tailwind",
+              "Google AI Studio",
+              "ElevenLabs",
+            ]}
+          >
+            <div className="gap-x-[5%] grid grid-cols-[40%,1fr] grid-rows-2 p-[7.5%] h-full [perspective:750px]">
+              <img
+                src="projects/drakes_diss.png"
+                alt="A screenshot from the project."
+                className="shadow-top-left mt-auto mb-4 rounded"
+              />
+              <img
+                src="projects/kendricks_critique.png"
+                alt="A screenshot from the project."
+                className="shadow-bottom-left col-start-1 mt-4 rounded"
+              />
+              <p
+                id="card-text"
+                className="col-start-2 row-span-2 row-start-1 my-auto text-3xl"
+              >
+                Resume Roast takes the user's resume, and uses AI to generate a
+                track by Drake, which disses the user's resume in a personalized
+                and playful manner. Drake will lay down some smooth bars and
+                sharp lyrics highlighting the weaknesses in your resume. After
+                listening to the exceptional track by Toronto's very own, the
+                user is then prompted to "fix their resume." They are then
+                treated to yet another musical masterpiece by Drake's biggest
+                rival, Kendrick Lamar.
+              </p>
+            </div>
+          </Card>
+          <Card
+            name="Code Genius"
+            href="https://github.com/MatthewFrieri/HackThe6ix-CodeGenius"
+            tools={[
+              "NextJS",
+              " TypeScript ",
+              "Tailwind",
+              "Flask",
+              "Meta Llama",
+            ]}
+          >
+            <div className="gap-x-[5%] grid grid-cols-[40%,1fr] grid-rows-2 p-[7.5%] h-full [perspective:750px]">
+              <img
+                src="projects/code_review.png"
+                alt="A screenshot from the project."
+                className="shadow-top-left mt-auto mb-4 rounded"
+              />
+              <img
+                src="projects/why_score.png"
+                alt="A screenshot from the project."
+                className="shadow-bottom-left col-start-1 mt-4 rounded"
+              />
+              <p
+                id="card-text"
+                className="col-start-2 row-span-2 row-start-1 my-auto text-3xl"
+              >
+                CodeGenius starts with the user input. Users can drag-and-drop
+                either a singular code file or a larger .zip file with many
+                different code files in it. It can parse most modern programming
+                languages, but our models have been tested and prompted with
+                mostly Python3 code. Then, in seconds all files provided by the
+                user are annotated! Certain sections of the program are
+                highlighted as areas of potential confusion for someone else
+                reading it. Then, upon either hovering over (or clicking on) the
+                section of code, a more detailed annotation is provided.
+              </p>
+            </div>
+          </Card>
+          <Card
+            name="Cook Clever"
+            href="https://github.com/MatthewFrieri/HawkHacks_CookClever"
+            tools={["React", "Flask", "Google AI Studio", "MongoDB", "Neurelo"]}
+          >
+            <div className="flex items-center gap-x-[4%] px-[3%] py-[7.5%] h-full [perspective:750px]">
+              <img
+                src="projects/login.png"
+                alt="A screenshot from the project."
+                className="shadow-left rounded w-[25%]"
+              />
+              <img
+                src="projects/image_analysis.png"
+                alt="A screenshot from the project."
+                className="shadow-left rounded w-[25%]"
+              />
+              <p id="card-text" className="my-auto text-3xl">
+                CookClever is an innovative web app designed to help people
+                learn how to cook. It offers a wide range of recipes with
+                detailed, step-by-step instructions to guide users through the
+                cooking process. At each step, users are prompted to take a
+                picture of their progress. Using AI, CookClever analyzes these
+                images and provides tailored feedback to help users improve
+                their cooking skills.
+              </p>
+            </div>
+          </Card>
+        </div>
+      </section>
+
+      <section className="bg-dark">
+        <div className="flex justify-center items-center gap-8 h-60 text-4xl container">
+          <div className="border-secondary mr-10 border w-80" />
+          <Link
+            href={"mailto: mat.frieri@gmail.com"}
+            target="_blank"
+            className="hover:text-secondary transition-all hover:scale-110"
+          >
+            <FontAwesomeIcon icon={faEnvelope} />
+          </Link>
+          <Link
+            href={"https://github.com/MatthewFrieri"}
+            target="_blank"
+            className="hover:text-secondary transition-all hover:scale-110"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </Link>
+          <Link
+            href={"https://www.linkedin.com/in/matthew-frieri"}
+            target="_blank"
+            className="hover:text-secondary transition-all hover:scale-110"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </Link>
+          <Link
+            href={"Resume.pdf"}
+            target="_blank"
+            className="hover:text-secondary transition-all hover:scale-110"
+          >
+            <FontAwesomeIcon icon={faNewspaper} />
+          </Link>
+          <div className="border-secondary ml-10 border w-80" />
+        </div>
       </section>
     </>
   );
